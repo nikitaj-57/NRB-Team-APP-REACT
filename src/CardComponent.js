c57eb21c6e7c7defef3b95a7bffa4ae852140944
nikitaj-57 <nikitajit2@gmail.com>
2023-01-31 12:23:56 +0530

    created cards for team members

--- src/CardComponent.js
+++ src/CardComponent.js
@@ -1,7 +1,23 @@
 import React from "react";
 
-const CardComponent = () => {
-  return <div>CardComponent</div>;
+const CardComponent = (props) => {
+  const { name, img, company, role, location } = props.user;
+  return (
+    <div className="mx-5 my-5 border-2 border-orange-400 gap-2 items-center flex">
+      <div>
+        <img className="h-40 w-44" src={img} alt="" />
+      </div>
+      <div>
+        <div className="flex">
+          <h1>Name : {name}</h1>
+          {/* <img src="" alt="github logo" /> */}
+        </div>
+        <h2>Designation : {role}</h2>
+        <h3>Company : {company}</h3>
+        <h4>Location : {location}</h4>
+      </div>
+    </div>
+  );
 };
 
 export default CardComponent;
--- src/CardContainer.js
+++ src/CardContainer.js
@@ -1,10 +1,13 @@
 import React from "react";
 import CardComponent from "./CardComponent";
+import data from "./data/data";
 
 const CardContainer = () => {
   return (
-    <div>
-      <CardComponent />
+    <div className="grid lg:grid-cols-3">
+      {data.map((user) => (
+        <CardComponent key={user.id} user={user} />
+      ))}
     </div>
   );
 };
